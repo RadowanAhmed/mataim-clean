842bcf35a8e43e2b0673f2cc2552c8d9ea4d1df7
Radowan Ahmed SharafAldeen Adam <92976438+RadowanAhmed@users.noreply.github.com>
2026-02-26 18:44:57 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // OpenRouteService API Key (FREE - get yours at https://openrouteservice.org/)
 export const ORS_API_KEY =
-  "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6Ijc4OWYxYjU3MTdjNDQ4NGFiMjVjNWExZDU0MWI1ZWIxIiwiaCI6Im11cm11cjY0In0="; // Replace with your own free key
+  ""; // Replace with your own free key
